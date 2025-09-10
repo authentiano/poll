b9655e10162d9ba6b7a10b63934e908987870345
authentiano <2400810220@std.kyu.ac.ug>
2025-09-10 23:59:01 +0300

    Database rectification

--- djangoproject/polls/models.py
+++ djangoproject/polls/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Questions(models.model):
+class Questions(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
-class Choice(models.model):
+class Choice(models.Model):
     question = models.ForeignKey(Questions, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length= 100)
     votes =models.IntegerField(default = 0)
