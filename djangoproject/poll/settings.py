e83adfe7d1ca70d41f94b2d1fad924d0eff5f141
authentiano <emmasanauthentiano@gmail.com>
2025-09-10 17:59:47 +0000

    Database creaction

--- djangoproject/poll/settings.py
+++ djangoproject/poll/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "polls"
+
 ]
 
 MIDDLEWARE = [
--- djangoproject/polls/models.py
+++ djangoproject/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Questions(models.model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.model):
+    question = models.ForeignKey(Questions, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length= 100)
+    votes =models.IntegerField(default = 0)
+    
